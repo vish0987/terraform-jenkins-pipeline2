932968794b4429a2d374524277875158c8842cd9
vish0987 <vishvendra.acumen@gmail.com>
2025-07-29 00:30:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,23 @@
-pipeline {
-   agent any
-   tools {
-        terraform "terraform_1.12.2"
-    }
+provider "local" {}
 
-   stages {
-        stage('Clone Repo') {
-            steps {
-              git url : 'https://github.com/vish0987/terraform-jenkins-pipeline2.git' , branch : 'master'
-              }
-           }
-        stage('terraform init') {
-            steps {
-               sh 'terraform init'
-              }
-            }
-        stage('terraform validate') {
-            steps {
-               sh 'terraform validate'
-              }
-            }
-        stage('terraform plan') {
-            steps {
-               sh 'terraform plan'
-              }
-            }
-        stage('terraform apply') {
-            steps {
-               sh 'terraform apply -auto-approve'
-              }
-            }
+# Create directories
+resource "local_file" "directory_1" {
+  filename = "${path.module}/output/dir1/.keep"
+  content  = "This is a placeholder for dir1"
 }
+
+resource "local_file" "directory_2" {
+  filename = "${path.module}/output/dir2/.keep"
+  content  = "This is a placeholder for dir2"
+}
+
+# Create files
+resource "local_file" "file_1" {
+  filename = "${path.module}/output/file1.txt"
+  content  = "This is file 1"
+}
+
+resource "local_file" "file_2" {
+  filename = "${path.module}/output/file2.txt"
+  content  = "This is file 2"
 }
